80cd77645bc86dd5a245d7703bee2f276c51c9e2
shadow421 <shees.usman421@gmail.com>
2017-03-01 15:54:19 +0500

    if fixed

--- reader.py
+++ reader.py
@@ -6,7 +6,7 @@ def msgpktformer():
         for num,line in enumerate(myfile,1):
                 if '::::' in line:
                     linenum= num                
-        if (linenum!= -1)
+        if (linenum!= -1):
             for mynum,myline in enumerate(myfile,1):
                 if mynum > linenum:
                     message+=myline
