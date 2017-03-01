c488886e18762feb83541b4f40479cc7938d751d
shadow421 <shees.usman421@gmail.com>
2017-03-01 15:52:35 +0500

    with open fixed

--- reader.py
+++ reader.py
@@ -2,7 +2,7 @@ def msgpktformer():
     linenum = -1
     message = ""
 
-    with open('wsndata.txt','r') as myfile
+    with open('wsndata.txt','r') as myfile:
         for num,line in enumerate(myfile,1):
                 if '::::' in line:
                     linenum= num                
