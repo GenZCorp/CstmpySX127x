ac141af8d33d4904ef0586047800199a8cbde82d
shadow421 <shees.usman421@gmail.com>
2017-03-04 19:39:11 +0500

    holder

--- rx_cont.py
+++ rx_cont.py
@@ -50,8 +50,8 @@ class LoRaRcvCont(LoRa):
         print("\nRxDone")
         self.clear_irq_flags(RxDone=1)
         payload = self.read_payload(nocheck=True) #append data to file
-        print(bytes(payload[0]).decode())
-        if (bytes(payload[0]).decode()== NETWORK_KEY): #this isnt reading 35
+        holder = (bytes(payload[0]).decode())
+        if (holder == NETWORK_KEY): #this isnt reading 35
             if(RUN_ONE==1):
                 myfile = open('wsndata.txt','a') 
                 myfile.write("::::\n" + starttime + "\n") 
