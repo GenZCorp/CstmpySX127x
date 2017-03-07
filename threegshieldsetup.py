f8d43283f0896751a85e3419cbde4d8534b8cf23
shadow421 <shees.usman421@gmail.com>
2017-03-07 11:44:52 +0500

    debug

--- threegshieldsetup.py
+++ threegshieldsetup.py
@@ -3,7 +3,7 @@ import time
 
 KEY = "P8_15" # BB GPIO 66 in
 PS =  "P8_17" # BB GPIO 69 in
-
+i = 0
 GPIO.setup(KEY,GPIO.OUT)
 GPIO.setup(PS,GPIO.IN)
 GPIO.output(KEY,1)
@@ -11,14 +11,16 @@ GPIO.output(KEY,1)
 
 def powerup():
     print("3g power up")
+    global i
     while(GPIO.input(PS)!=1):
+        print(bytes(i))
         GPIO.output(KEY,0)
         print("delay begin")
         time.sleep(5)
         print("delay end")
         GPIO.output(KEY,1)
         print(bytes(GPIO.input(PS)))
-
+        i+=1
 
 def powerdown():
     print("3g power down")
