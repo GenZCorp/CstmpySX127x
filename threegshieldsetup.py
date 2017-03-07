b806d5ddbc27accfb7bca07525a99762e22fce32
shadow421 <shees.usman421@gmail.com>
2017-03-07 12:01:15 +0500

    debug excessive

--- threegshieldsetup.py
+++ threegshieldsetup.py
@@ -12,19 +12,20 @@ GPIO.output(KEY,1)
 def powerup():
     print("3g power up")
     global i
-    while(GPIO.input(PS)!=1):
+    while(!(GPIO.input(PS))):
         print(bytes(i))
         GPIO.output(KEY,0)
         print("delay begin")
         time.sleep(3)
         print("delay end")
+        print(bytes(GPIO.input(PS)))
         GPIO.output(KEY,1)
         print(bytes(GPIO.input(PS)))
         i+=1
 
 def powerdown():
     print("3g power down")
-    while(GPIO.input(PS)!=0):
+    while(GPIO.input(PS)):
         GPIO.output(KEY,0)
         print("down delay begin")
         time.sleep(3)
