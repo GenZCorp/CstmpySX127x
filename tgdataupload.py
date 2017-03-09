45b7e81984ef161d63fe79b2c76abc0752151e38
shadow421 <shees.usman421@gmail.com>
2017-03-09 19:42:15 +0500

    fixed url parse

--- tgdataupload.py
+++ tgdataupload.py
@@ -9,24 +9,24 @@ def threegupload():
     parser = message.split("\n")
     for mydatastring in parser:
         (tag,implier,loc1,loc2,sensor1data,sensor2data,sensor3data,sensor4data,sensor5data,sensor6data,sensor7data,sensor8data) = mydecoder.msgdecoder(mydatastring)
-        urllib.urlopen('https://api.thingspeak.com/update?api_key=TWLSAPS8KORLDE1I&field1='+tag)
-        urllib.urlopen('https://api.thingspeak.com/update?api_key=TWLSAPS8KORLDE1I&field2='+implier)
-        urllib.urlopen('https://api.thingspeak.com/update?api_key=TWLSAPS8KORLDE1I&field3='+loc1)
-        urllib.urlopen('https://api.thingspeak.com/update?api_key=TWLSAPS8KORLDE1I&field4='+loc2)
+        urllib.urlopen('https://api.thingspeak.com/update?api_key=TWLSAPS8KORLDE1I&field1='+bytes(tag))
+        urllib.urlopen('https://api.thingspeak.com/update?api_key=TWLSAPS8KORLDE1I&field2='+bytes(implier)
+        urllib.urlopen('https://api.thingspeak.com/update?api_key=TWLSAPS8KORLDE1I&field3='+bytes(loc1))
+        urllib.urlopen('https://api.thingspeak.com/update?api_key=TWLSAPS8KORLDE1I&field4='+bytes(loc2))
         if(sensor1data != -1):
-            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field1='+sensor1data)
+            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field1='+bytes(sensor1data))
         if(sensor2data != -1):
-            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field2='+sensor2data)
+            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field2='+bytes(sensor2data))
         if(sensor3data != -1):
-            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field3='+sensor3data)
+            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field3='+bytes(sensor3data))
         if(sensor4data != -1):
-            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field4='+sensor4data)
+            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field4='+bytes(sensor4data))
         if(sensor5data != -1):
-            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field5='+sensor5data)
+            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field5='+bytes(sensor5data))
         if(sensor6data != -1):
-            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field6='+sensor6data)
+            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field6='+bytes(sensor6data))
         if(sensor7data != -1):
-            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field7='+sensor7data)
+            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field7='+bytes(sensor7data))
         if(sensor8data != -1):
-            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field8='+sensor8data)
+            urllib.urlopen('https://api.thingspeak.com/update?api_key=2T4XCPR0G1SRDCBG&field8='+bytes(sensor8data))
         
